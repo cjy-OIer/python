690da4b26f5233daacb56c5071d32e335d9113c6
Tribios <cjy114514@outlook.com>
2025-08-15 09:50:45 +0800

    er

--- api/index.py
+++ api/index.py
@@ -98,7 +98,7 @@ class handler(BaseHTTPRequestHandler):
             data={"rslt":"No such user!"}
         '''
         #json_data = json.dumps(data).encode('utf-8')
-        json_data = json.dumps(body2).encode('utf-8')
+        json_data = json.dumps(body2)#.encode('utf-8')
         self.send_response(200)
         self.send_header('Content-type','application/json')
         self.end_headers()
